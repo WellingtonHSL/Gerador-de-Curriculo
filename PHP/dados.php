1b5c087be9a4328c5addc533af1d21b648892be1
shoityn <glaubershoity@gmail.com>
2024-08-25 18:42:40 -0300

    criei as variáveis para receber dados do formulário(dados_curriculo.html) das sessões dados pessoais e objetivo

--- PHP/dados.php
+++ PHP/dados.php
@@ -1,3 +1,16 @@
 <?php
     //TODO: Adicionar lógica para salvar os dados enviados
+    //DADOS PESSOAIS
+    $full_name = $_POST['full_name'];
+    $dt_birth = $_POST['dt_birth'];
+    $age = $_POST['age'];
+    $sexo = $_POST['sexo'];
+    $adress = $_POST['adress'];
+    $city = $_POST['city'];
+    $state = $_POST['state'];
+    $phone_number = $_POST['phone_number'];
+    $email = $_POST['email'];
+
+    //OBJETIVO
+    $objective = $_POST['objective'];
 ?>
