e778b4839e884af4bba3a52f2949e3ff9ee72087
Kamilla <143965348+kamillabarrros@users.noreply.github.com>
2024-08-25 18:52:36 -0300

    Adicionei as váriaveis para armazenar os dados enviados pelo (dados_currilo.html) das sessões (Formação Acadêmica e Experiencia Profissionais)

--- PHP/dados.php
+++ PHP/dados.php
@@ -1,3 +1,18 @@
 <?php
     //TODO: Adicionar lógica para salvar os dados enviados
+
+
+    //FORMAÇÃO ACABEMICA 
+    $course = $_POST['course'];
+    $institution = $_POST['institution'];
+    $ins_start_date = $_POST['ins_start_date'];
+    $ins_end_date = $_POST['ins_end_date'];
+
+    //EXPERIENCIA PROFISSIONAL
+    $company_name = $_POST['company_name'];
+    $company_locality = $_POST['company_locality'];
+    $job_activities = $_POST['job_activities'];
+    $comp_start_date = $_POST['comp_start_date'];
+    $comp_end_date = $_POST['$comp_end_date'];
+   
 ?>
