09b33715d65f1d0cfeed4686cea83dc6851b1b81
Wellington Henrique <wellingtonh_2012@hotmail.com>
2024-08-28 10:20:49 -0300

    adicionei o SessionStart() no dados.php e curriculo.php para levar as informações cedida para a geração do curriculo. Além disso, atualizei o dados_curriculo.html adicionando a tag <fieldset> e outras pequenas atulizações, como, por exemplo, as values do sexo e linguages. Criei mais 2 arquivos .css para que pudessemos estilizar cada pagina de forma individual.

--- PHP/curriculo.php
+++ PHP/curriculo.php
@@ -1,5 +1,43 @@
 <?php
     //TODO: Adicionar lógica para receber as os dados fornecidos no arquivo dados.php
+    session_start();
+
+    //DADOS PESSOAIS
+    $full_name = $_SESSION['full_name'];
+    $dt_birth = $_SESSION['dt_birth'];
+    $age = $_SESSION['age'];
+    $sexo = $_SESSION['sexo'];
+    $address = $_SESSION['address'];
+    $city = $_SESSION['city'];
+    $state = $_SESSION['state'];
+    $phone_number = $_SESSION['phone_number'];
+    $email = $_SESSION['email'];
+
+    //OBJETIVOS
+    $objective = $_SESSION['objective'] ?? '';
+
+    //FORMAÇÃO ACADEMICA
+    $course = $_SESSION['course'] ?? '';
+    $institution = $_SESSION['institution'] ?? '';
+    $ins_start_date = $_SESSION['ins_start_date'] ?? '';
+    $ins_end_date = $_SESSION['ins_end_date'] ?? '';
+
+    //EXPERIENCIA PROFISSIONAL
+    $company_name = $_SESSION['company_name'] ?? '';
+    $company_locality = $_SESSION['company_locality'] ?? '';
+    $job_activities = $_SESSION['job_activities'] ?? '';
+    $comp_start_date = $_SESSION['comp_start_date'] ?? '';
+    $comp_end_date = $_SESSION['comp_end_date'] ?? '';
+
+    //ATIVIDADES COMPLEMENTARES
+    $activity_title = $_SESSION['activity_title'] ?? '';
+    $atv_start_date = $_SESSION['atv_start_date'] ?? '';
+    $atv_end_date = $_SESSION['atv_end_date'] ?? '';
+    $skills = $_SESSION['skills'] ?? '';
+
+    //IDIOMAS
+    $languages = $_SESSION['languages'] ?? '';
+    $fluency = $_SESSION['fluency'] ?? '';
 ?>
 
 <!DOCTYPE html>
@@ -7,11 +45,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="CSS/curriculo.css" media="screen" />
+    <title>TalentTail</title>
 </head>
 <body>
     <!--TODO: Adicionar as informaçoes do curriculo-->
-    <!--TODO: Adicionar botao para imprimir o curriculo-->
+    <!--TODO: Adicionar botao para imprimir o curriculo ou editar-->
     <main>
     </main>
     
